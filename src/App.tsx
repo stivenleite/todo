56738e76742b622b54231bde0e64eff4cd709fcd
Stiven Leite <stivenleite@gmail.com>
2024-04-24 12:37:06 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import logo from "./assets/Logo.png"
+
+export function App () {
+  return (
+    <div>
+      <header className="bg-gray-700 h-[200px] flex items-center justify-center">
+        <img src={logo} alt="Logo" className="h-12"/>
+      </header>
+
+      <main>
+        
+      </main>
+    </div>
+  )
+}
